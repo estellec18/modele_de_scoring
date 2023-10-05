89cff5141c6809a7d87fbef163d2f0c1f2938242
estellec18 <estelle.cmps@gmail.com>
2023-10-05 14:37:38 +0200

    prediction.py deleted (no longer needed

--- prediction.py
+++
@@ -1,8 +0,0 @@
-import joblib
-import numpy as np
-
-def predict(data):
-    clf = joblib("pipeline_logreg_best.joblib")
-    classes = {0:"Client ok",1:"Default"}
-    preds = clf.predict_proba([data])[0]
-    return (classes[np.argmax(preds)], preds)
